c6377a1da5f13a61d518c66f0da533a65a23506e
abhijeet <abhijeetsinha43@gmail.com>
2022-12-31 20:40:33 +0530

    feat: react elements

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+//Adding elements in dom through react
+
+const heading1 = React.createElement("h1", {}, "Inception Session");
+
+const heading2 = React.createElement("h1", { id: "title" }, "Welcome");
+
+const container = React.createElement("div", { className: "container" }, [
+  heading1,
+  heading2,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
 // JS
 // const root = document.getElementById("root");
 // const heading = document.createElement("h1");

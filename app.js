c112a4376abc81464d98127802d7477671cdfcbe
abhijeet <abhijeetsinha43@gmail.com>
2022-12-31 20:31:02 +0530

    feat: added react cdn links

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const root = document.getElementById("root");
-
-const heading = document.createElement("h1");
-
-heading.innerHTML = "Inception Session";
-
-root.appendChild(heading);
+// JS
+// const root = document.getElementById("root");
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Inception Session";
+// root.appendChild(heading);
